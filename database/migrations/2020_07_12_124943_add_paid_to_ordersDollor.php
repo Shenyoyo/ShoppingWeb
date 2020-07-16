088b0b05ee80b57a194455d2e2e452dabc33055f
Yoyo_Shen <yoyo_shen@mail.chungyo.net>
2020-07-16 09:33:49 +0800

    更新migrate

--- database/migrations/2020_07_12_124943_add_paid_to_ordersDollor.php
+++ database/migrations/2020_07_12_124943_add_paid_to_ordersDollor.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddPaidToOrders extends Migration
+class AddPaidToOrdersDollor extends Migration
 {
     /**
      * Run the migrations.
