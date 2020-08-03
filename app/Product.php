5906dfdff0d3655964614ca115a4ea6d8fc1f3dc
Yoyo_Shen <yoyo_shen@mail.chungyo.net>
2020-08-03 14:05:03 +0800

    修改錯誤

--- app/Product.php
+++ app/Product.php
@@ -3,12 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-
-    use SoftDeletes;
     
+    use SoftDeletes;
+
     public function file()
     {
         return $this->belongsTo('App\File');
